45fab4a54791d2b970033f3be36c6b707c7105a9
Your Name <kaser.slaiby@gmail.com>
2023-12-15 20:12:08 +0300

    //Remove Actionbar From Just Splash

--- app/src/main/java/com/example/task_management/MainActivity.java
+++ app/src/main/java/com/example/task_management/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.task_management;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        style for layout bar
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_barlayout);
+
     }
 }
--- app/src/main/java/com/example/task_management/SplashActivity.java
+++ app/src/main/java/com/example/task_management/SplashActivity.java
@@ -5,12 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Remove Actionbar From Just Splash
+        // Take instance of Action Bar
+        // using getSupportActionBar and
+        // if it is not Null
+        // then call hide function
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
         setContentView(R.layout.activity_splash);
         new Handler().postDelayed(new Runnable() {
             @Override
