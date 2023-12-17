e21f6830c7941e8d15cecacd1e8638d4ea2defe6
kaser1997 <kaser.slaiby@gmail.com>
2023-12-17 14:45:00 +0300

    add menu

--- app/src/main/java/com/example/task_management/MainActivity.java
+++ app/src/main/java/com/example/task_management/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,10 +18,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private DBHandler dbHandler;
 
+    final int MENU_ABOUT_ID = 1;
+    final int MENU_QUIT_ID = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//         initializing all our objects
+        btnAdd = findViewById(R.id.btnAdd);
+
+//        below line is to add onclick listener for our buttons
+        btnAdd.setOnClickListener(this);
+
+//      creating a new handler class
+        dbHandler = new DBHandler(MainActivity.this);
+
+        readAllData();
 
 //        style for layout bar
         Objects.requireNonNull(getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
@@ -28,13 +43,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+//      read all Categories records
+    public void readAllData() {
+
+    }
+
     @Override
     public void onClick(View v) {
 
-        //adding new Categories
+//        adding new Categories
     if (v == btnAdd) {
 
     }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, MENU_ABOUT_ID, 0, "About programmers");
+        menu.add(0, MENU_QUIT_ID, 0, "Close the application");
+        return super.onCreateOptionsMenu(menu);
     }
 }
