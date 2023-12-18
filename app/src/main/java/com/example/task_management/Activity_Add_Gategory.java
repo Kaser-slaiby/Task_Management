d0298a37ee97205ff1c2867f393e7819fd8295e6
Your Name <kaser.slaiby@gmail.com>
2023-12-18 19:33:38 +0300

    new Activity for add

---
+++ app/src/main/java/com/example/task_management/Activity_Add_Gategory.java
@@ -0,0 +1,14 @@
+package com.example.task_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Add_Gategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_gategory);
+    }
+}
